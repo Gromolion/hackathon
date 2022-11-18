849e2a3dced33e73f7a38fd16c3042a0761f2f17
Alexandr Shaporov <a.shaporov@worksolutions.ru>
2022-11-18 20:38:29 +0300

    fix

--- project/frontend/quasar.config.js
+++ project/frontend/quasar.config.js
@@ -29,8 +29,7 @@ module.exports = configure(function (/* ctx */) {
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
     boot: [
-      'i18n',
-      'axios'
+      'services'
     ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#css
