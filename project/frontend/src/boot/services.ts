12452277766a89b13d53700013ed0495f906dd07
Alexandr Shaporov <a.shaporov@worksolutions.ru>
2022-11-18 16:05:43 +0300

    fixed some mistypes

--- project/frontend/src/boot/services.ts
+++ project/frontend/src/boot/services.ts
@@ -1,11 +1,13 @@
 import { boot } from 'quasar/wrappers'
 
 import services from 'src/services'
+import { inject } from 'vue'
 
-declare module 'services' {
-}
-/* eslint-enable @typescript-eslint/no-empty-interface */
+const APP_SERVICES_TOKEN = 'APP_SERVICES_TOKEN'
 
 export default boot(({ app }) => {
   app.config.globalProperties.services = services
+  app.provide(APP_SERVICES_TOKEN, services)
 })
+
+export const useServices = () => inject(APP_SERVICES_TOKEN)
